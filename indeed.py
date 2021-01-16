a6da9d292dfbf1eb89b24426b00912d9d88c5e30
Hedsw <yun.hyeok.lee@gmail.com>
2021-01-16 16:54:30 -0500

    Python Script

--- indeed.py
+++ indeed.py
@@ -4,18 +4,14 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = "https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
-    
     soup = BeautifulSoup(result.text, "html.parser")
-
     pagination = soup.find("div", {"class": "pagination"})
-
     links = pagination.find_all('a')
     pages = []
     for link in links[:-1]:
         pages.append(int(link.string))
-
     max_page = pages[-1]
     return max_page
 
@@ -23,17 +19,20 @@ def extract_indeed_pages():
 def extract_job(html):
     title = html.find("h2", {"class": "title"}).find("a")["title"]
     company = html.find("span", {"class": "company"})
-    company_anchor = company.find("a")
-    if company_anchor is not None:
-        company = str(company.find("a").string)
+    if company:
+        company_anchor = company.find("a")
+        if company_anchor is not None:
+            company = str(company.find("a").string)
+        else:
+            company = str(company.string)
+        company = company.strip()
     else:
-        company = str(company.string)
-    company = company.strip()
+        company = None
     location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
     job_id = html["data-jk"]
     return {'title': title, 'company': company, 'location': location, "link": f"https://www.indeed.com/viewjob?jk={job_id}"}
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping page {page}")
@@ -45,3 +44,7 @@ def extract_indeed_jobs(last_page):
             jobs.append(job)
     return jobs
 
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
+    return jobs
