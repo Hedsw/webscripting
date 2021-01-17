45fadefd63c0ab1e45a2a87bed0774190cc66627
Hedsw <yun.hyeok.lee@gmail.com>
2021-01-17 18:43:32 -0500

    Separate to multiple file

--- so.py
+++ so.py
@@ -3,3 +3,7 @@ from bs4 import BeautifulSoup
 
 LIMIT = 50
 URL = "https://www.indeed.com/jobs?q=python&limit={LIMIT}"
+
+
+# 2-9 StackOverFlow Pages 4:50초까지 봤음 영상 니콜라스 영상 
+# https://nomadcoders.co/python-for-beginners/lectures/126
