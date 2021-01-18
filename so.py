7625634a2cfc772f911f1263b6fe1b7b190bc339
Hedsw <yun.hyeok.lee@gmail.com>
2021-01-17 20:33:53 -0500

    add stackoverflow and indeed at once

--- so.py
+++ so.py
@@ -11,14 +11,31 @@ def get_lage_page():
     last_page = pages[-2].get_text(strip=True)
     return int(last_page)
 
+def extract_job(html):
+    title = html.find("h2", {"class": "mb4"}).find("a")["title"]
+    company, location = html.find("h3", {"class": "fc-black-700"}).find_all("span", recursive = 0)
+    job_id = html.find("h2",{"class":"fs-body3"}).find("a")["href"]
+    #0 = False, 1 = True
+    return {
+        "title": title,
+        "company": company,
+        "location": location,
+        "apply_link": f"https://stackoverflow.com{job_id}"
+    }
+
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"Scrapping Stackoverflow Page: {page}")
         result = requests.get(f"{URL}&pg={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class":"-job"})
         for result in results:
-            print(result["data-jobid"])
+            #print(result["data-jobid"])
+            job = extract_job(result)
+            #print(job)
+            jobs.append(job)
+    return jobs
 
 def get_jobs():
     last_page = get_lage_page()
@@ -28,3 +45,6 @@ def get_jobs():
 
 # 2-9 StackOverFlow Pages 4:50초까지 봤음 영상 니콜라스 영상 
 # https://nomadcoders.co/python-for-beginners/lectures/126
+
+#송용남 차장
+#055-330-7661
