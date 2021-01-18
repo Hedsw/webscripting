d4ac29cd7b5309c801b9cd678992ff539df08f68
Hedsw <yun.hyeok.lee@gmail.com>
2021-01-17 19:01:51 -0500

    stack oveflow scrapting

--- so.py
+++ so.py
@@ -2,7 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 
 LIMIT = 50
-URL = "https://www.indeed.com/jobs?q=python&limit={LIMIT}"
+URL = "https://stackoverflow.com/jobs?q=python"
+
+def get_lage_page():
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+    print(pages)
+
+def get_jobs():
+    last_page = get_lage_page()
+    return[]
 
 
 # 2-9 StackOverFlow Pages 4:50초까지 봤음 영상 니콜라스 영상 
